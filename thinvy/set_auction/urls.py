9219beb6b13aa75363375879423ef3ea3b2f0be1
yoyokazoo <peter.beckfield@gmail.com>
2020-12-31 13:24:38 -0800

    Example index and detail views for sets (final product will look much different).

---
+++ thinvy/set_auction/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path("", views.set_auction_index, name="set_auction_index"),
+	path("<int:pk>/", views.set_auction_detail, name="set_auction_detail"),
+]
--- thinvy/set_auction/views.py
+++ thinvy/set_auction/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from set_auction.models import SetDraftingSchedule
 
-# Create your views here.
+def set_auction_index(request):
+	set_auction = SetDraftingSchedule.objects.all()
+	context = {
+		'set_auction': set_auction
+	}
+	return render(request, 'set_auction_index.html', context)
+
+def set_auction_detail(request, pk):
+	set_auction = SetDraftingSchedule.objects.get(pk=pk)
+	context = {
+		'set_auction': set_auction
+	}
+	return render(request, 'set_auction_detail.html', context)
